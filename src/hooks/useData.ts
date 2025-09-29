629e849667fdca2b907cf5e8e5c99d5005ead9b1
Umar <umarisakov0006@gmail.com>
2025-09-29 18:48:48 +0600

    Filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 
@@ -10,7 +10,7 @@ interface fetchResponse<T> {
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(true)
@@ -21,7 +21,7 @@ const useData = <T>(endpoint: string) => {
 
     console.log("Fetching genres...");
     apiClient
-        .get<fetchResponse<T>>(endpoint, {signal: controller.signal})
+        .get<fetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
         .then((res) => {setData(res.data.results); setLoading(false)})
         .catch((err) => {
         if (err instanceof CanceledError) return;
@@ -29,7 +29,7 @@ const useData = <T>(endpoint: string) => {
         }});
 
     return () => controller.abort();
-    }, []);
+    }, deps ? [...deps] : []);
     return {data, error, isLoading}  
 };
 

250306a5928684f792d02f4c405fe87af6b46bb2
Umar <umarisakov0006@gmail.com>
2025-09-30 18:35:48 +0600

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,6 +17,7 @@ export interface Game {
   background_image: string;
   parent_platforms: {platform: Platform}[]
   metacritic: number
+  rating_top: number
 }
 
 
@@ -28,7 +29,8 @@ const useGames = (
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
+        search: gameQuery.searchText,
+        
       }},
       [gameQuery]);
 
